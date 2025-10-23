c88b325cb65528990e7d73ebd4b86adf996534ce
Hanbing Liu <lizliu@Hanbings-MacBook-Air.local>
2025-10-23 12:44:21 -0700

    Remove Firebase Analytics to fix loading issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtf7_ek5gVI-bx0tUXLQvRz6mZcKfcjf4",
@@ -20,4 +19,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-export const analytics = getAnalytics(app);
